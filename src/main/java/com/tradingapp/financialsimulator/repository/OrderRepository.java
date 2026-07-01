3abeeaa8edf46b074a99e4d3cce6b6c9db233d0f
SumitAdbane02 <sumitadbane02@gmail.com>
2026-07-01 23:22:59 +0530

    Create a Spring REST Controller for Placing Market Orders

--- src/main/java/com/tradingapp/financialsimulator/repository/OrderRepository.java
+++ src/main/java/com/tradingapp/financialsimulator/repository/OrderRepository.java
@@ -3,15 +3,13 @@ package com.tradingapp.financialsimulator.repository;
 import com.tradingapp.financialsimulator.model.Order;
 import com.tradingapp.financialsimulator.model.OrderStatus;
 import com.tradingapp.financialsimulator.model.User;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
-public interface OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
     List<Order> findByUser(User user);
 
     List<Order> findAllByStatus(OrderStatus status);
-
-    Order save(Order order);
 }
